ea3d329939a92bc9131779bebeaba4d896f511bc
Samiaboug <bougtoubsamia@gmail.com>
2025-02-10 12:12:09 +0000

    save

---
+++ BOUGTOUB_Samia/partie1/database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'User 1',
+                'email' => 'user1@example.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'name' => 'User 2',
+                'email' => 'user2@example.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'name' => 'User 3',
+                'email' => 'user3@example.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ],
+        ];
+
+        foreach ($users as $user) {
+            \App\Models\User::create($user);
+        }
+    }
+}
