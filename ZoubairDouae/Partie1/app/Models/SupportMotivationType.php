c2f9f12b7bbf8b78746a795792c9eac96fdfa5f3
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2025-02-12 12:02:19 +0000

    partie1

---
+++ ZoubairDouae/Partie1/app/Models/SupportMotivationType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupportMotivationType extends Model
+{
+    protected $fillable = ['support_motivation_id', 'type_motivation_id'];
+}
