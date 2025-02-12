f6b85a02e4f8a91cd4c34a10094198fd4ddef78f
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2025-02-12 10:11:10 +0100

    Part1

--- SUIRITA_Fahd/Part1/app/Models/User.php
+++ SUIRITA_Fahd/Part1/app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function article()
+    {
+        return $this->hasOne(Article::class);
+    }
 }
