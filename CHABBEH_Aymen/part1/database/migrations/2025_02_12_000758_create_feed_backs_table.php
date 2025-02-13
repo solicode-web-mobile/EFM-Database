2176e34a97ecb63672f0adc610442a1b75559f17
ChabbehAymen <devaymen15.5.3@gmail.com>
2025-02-13 16:40:00 +0100

    part1

---
+++ CHABBEH_Aymen/part1/database/migrations/2025_02_12_000758_create_feed_backs_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feed_backs', function (Blueprint $table) {
+            $table->foreignId('avie_id')->constrained('avies', 'id')->onDelete('cascade');
+            $table->foreignId('feedback_type_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feed_backs');
+    }
+};
