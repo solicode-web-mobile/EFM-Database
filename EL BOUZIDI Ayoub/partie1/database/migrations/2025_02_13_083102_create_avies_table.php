5c228310ba3bfe76ce3d526cc6c6cf8a55ed85a3
Ayoub <ayoubsolicode07@gmail.com>
2025-02-13 12:53:20 +0000

    part1

--- EL BOUZIDI Ayoub/partie1/database/migrations/2025_02_13_083102_create_avies_table.php
+++ EL BOUZIDI Ayoub/partie1/database/migrations/2025_02_13_083102_create_avies_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('feedback', function (Blueprint $table) {
+        Schema::create('avies', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('strategy_id')->constrained()->onDelete('cascade');
+            $table->string('content');
+            $table->integer('vu');
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('feedback');
+        Schema::dropIfExists('avies');
     }
 };
--- EL BOUZIDI Ayoub/partie1/routes/web.php
+++ EL BOUZIDI Ayoub/partie1/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StrategyController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',[StrategyController::class , 'index']);
